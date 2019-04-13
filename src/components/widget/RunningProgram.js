c03f62abff997be606afdbbe6b5923e743f16a2e
Alvin Tanurwijaya <alvin8876@gmail.com>
2019-04-14 02:15:29 +0700

    Running Program widget

---
+++ src/components/widget/RunningProgram.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import styled from '@emotion/styled'
+import Text from '../../presentationals/Text'
+import { ViewWrapper, Wrapper } from '../../presentationals/index'
+
+const RunningProgram = (props) => {
+    const { eventName, numRegistered, numUnprocess, style } = props
+    return (
+        <ViewWrapper itemCenter flex={1} background={'#000'} spaceBetween>
+            <Wrapper>
+                <Wrapper column style={{ marginRight: 72 }}>
+                    <Text color={'white'} bold large>Running Program</Text>
+                    <Text color={'white'} small>{eventName}</Text>
+                </Wrapper>
+
+                <Wrapper column>
+                    <Text color={'white'} bold medium>{numRegistered}</Text>
+                    <Text color={'white'} small>Mendaftar</Text>
+                </Wrapper>
+
+                <Wrapper column>
+                    <Text color={'white'} bold medium>{numUnprocess}</Text>
+                    <Text color={'white'} small>Belum diproses</Text>
+                </Wrapper>
+            </Wrapper>
+
+            <ButtonRight onClick={() => alert('klik')}><b>Lihat Detail</b></ButtonRight>
+        </ViewWrapper>
+    )
+}
+
+const ButtonRight = styled.button(props => ({
+    display: 'flex',
+    heigt: 'auto',
+    right: 16,
+    background: '#fff',
+    padding: '16px 32px',
+    margin: 16,
+    ":hover": {
+        cursor: 'pointer'
+    }
+}))
+
+export default RunningProgram
